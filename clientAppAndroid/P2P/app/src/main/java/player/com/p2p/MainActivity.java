22cc1254775997e48e85ed4c27a3f817e17e928f
Gourab Saha <boyinyourbox@aol.com>
2019-04-17 01:35:42 +0530

    Socket Message communication and File upload intent binded in the activity

--- clientAppAndroid/P2P/app/src/main/java/player/com/p2p/MainActivity.java
+++ clientAppAndroid/P2P/app/src/main/java/player/com/p2p/MainActivity.java
@@ -1,7 +1,10 @@
 package player.com.p2p;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
@@ -10,6 +13,7 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +24,17 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.common.io.ByteStreams;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.handshake.ServerHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -28,13 +43,20 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import in.gauriinfotech.commons.Commons;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button discover, sendMsgBtn;
+    Button discover, sendMsgBtn, UploadBtn;
     ListView wifiDeviceList;
+    private static final int READ_REQUEST_CODE = 42;
 
     IntentFilter intentFilter;
     WifiP2pManager manager;
@@ -53,6 +75,11 @@ public class MainActivity extends AppCompatActivity {
 
     String message;
 
+    SocketServer masterSocketServer;
+    WebSocketClient masterClient;
+
+    int MASTER_ROLE; // 1 When Server, 0 When Client
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         discover = (Button) findViewById(R.id.wifi_discovery);
         sendMsgBtn = (Button) findViewById(R.id.wifi_send);
         wifiDeviceList = (ListView) findViewById(R.id.wifi_device_list);
+        UploadBtn = (Button) findViewById(R.id.wifi_upload);
 
         intentFilter = new IntentFilter();
 
@@ -128,23 +156,71 @@ public class MainActivity extends AppCompatActivity {
         sendMsgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        sendReceive.write(message.getBytes());
+                        Log.e("SOCKET MESSAGE", "sending message" + message);
+                        if(MASTER_ROLE == 1){
+                            // TODO: Find a way to send message to client
+                        }else{
+                            masterClient.send(message.getBytes());
+                        }
                     }
-                });
+                }).start();
             }
         });
+
+        UploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(intent, READ_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                //dumpFileMetaData(uri);
+
+                String realPath = Commons.getPath(data.getData(), getApplicationContext());
+                File ourFile = new File(realPath);
+                byte[] bytesArray = new byte[(int) ourFile.length()];
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(ourFile);
+                    fis.read(bytesArray); //read file into bytes[]
+                    fis.close();
+
+                    // Send the file through socket
+
+                    masterClient.send(bytesArray);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
+            String responseMessage = (String) msg.obj;
+            Log.e("RESONSE MSG", responseMessage);
             switch(msg.what){
                 case MESSAGE_READ:
                     byte[] readBuffer = (byte[]) msg.obj;
                     String tempMsg = new String(readBuffer, 0, msg.arg1);
+                    Log.d("SOCKET RESPONSE", tempMsg);
                     Toast.makeText(getApplicationContext(), "Message: " + tempMsg, Toast.LENGTH_SHORT).show();
                     break;
             }
@@ -194,18 +270,25 @@ public class MainActivity extends AppCompatActivity {
             if(wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner){
                 Toast.makeText(getApplicationContext(), "This is Server", Toast.LENGTH_SHORT).show();
                 message = "Server Sending message to Client";
+                MASTER_ROLE = 1;
 
                 /**Start the server thread*/
-                server = new Server();
+                server = new Server(groupOwnerAddress);
                 server.start();
 
             }else if(wifiP2pInfo.groupFormed){
                 Toast.makeText(getApplicationContext(), "This is Client", Toast.LENGTH_SHORT).show();
                 message = "Client sending message to Server";
+                MASTER_ROLE = 0;
 
                 /**Start the client thread*/
-                client = new Client(groupOwnerAddress);
-                client.start();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        client = new Client(groupOwnerAddress);
+                        client.start();
+                    }
+                }, 3000);
             }
         }
     };
@@ -224,23 +307,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class Server extends Thread {
-        Socket socket;
-        ServerSocket serverSocket;
+        SocketServer server;
+        String hostAddress;
+
+        public Server(InetAddress ownerAddress){
+            this.hostAddress = ownerAddress.getHostAddress();
+        }
 
         @Override
         public void run() {
-            try {
-                serverSocket = new ServerSocket(8888);
-                socket = serverSocket.accept();
+            if(masterSocketServer == null){
+                try {
+                    InetSocketAddress address  = new InetSocketAddress(hostAddress, 9999);
+                    this.server = new SocketServer(address);
+                    this.server.start();
+                    masterSocketServer = this.server;
+                    Log.d("SOCKET SERVER", "Server established");
 
-                /**Start the send receive thread*/
-                sendReceive = new SendReceive(socket);
-                sendReceive.start();
+                    /**Start the send receive thread*/
 
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -259,7 +347,15 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     bytes = inputStream.read(buffer);
                     if(bytes > 0){
-                        handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer);
+                        Log.d("SOCKET RESPONSE", "Response received");
+                        //handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer);
+                        Message msg = new Message();
+                        String message = new String(ByteStreams.toByteArray(inputStream));
+                        System.out.println("Response text is "+ message);
+                        msg.obj = message;
+                        msg.what = MESSAGE_READ;
+                        Log.d("SOCKET MESSAGE", message);
+                        handler.handleMessage(msg);
 
                     }
                 } catch (IOException e) {
@@ -288,25 +384,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class Client extends Thread {
-        Socket socket;
+        WebSocketClient socket;
         String hostAddress;
+        String CLIENT_SOCKET_TAG = "ClientSocket";
 
         public Client(InetAddress hostAddress){
             this.hostAddress = hostAddress.getHostAddress();
-            socket = new Socket();
-
         }
 
 
         @Override
         public void run() {
-            try {
-                socket.connect(new InetSocketAddress(this.hostAddress,8888), 500);
+            if(socket == null){
+                try {
+                    socket = new WebSocketClient(new URI("ws://" + this.hostAddress + ":9999")) {
+                        @Override
+                        public void onOpen(ServerHandshake handshakedata) {
+                            Log.e(CLIENT_SOCKET_TAG, " Connected");
+                        }
 
-                /**Code for sending message*/
-                /**Start the send receive thread*/
-                sendReceive = new SendReceive(socket);
-                sendReceive.start();
+                        @Override
+                        public void onMessage(String message) {
+                            Log.e(CLIENT_SOCKET_TAG, " Message: " + message);
+                        }
+
+                        @Override
+                        public void onClose(int code, String reason, boolean remote) {
+                            Log.e(CLIENT_SOCKET_TAG, " Disconnected");
+                        }
+
+                        @Override
+                        public void onError(Exception ex) {
+                            Log.e(CLIENT_SOCKET_TAG, " Error");
+                            ex.printStackTrace();
+                        }
+                    };
+                    socket.connect();
+                    masterClient = socket;
+//                socket.connect(new InetSocketAddress(this.hostAddress,8888), 500);
+//
+//                /**Code for sending message*/
+//                /**Start the send receive thread*/
+//                sendReceive = new SendReceive(socket);
+//                sendReceive.start();
 
 //                sendMsgBtn.setOnClickListener(new View.OnClickListener() {
 //                    @Override
@@ -315,9 +435,52 @@ public class MainActivity extends AppCompatActivity {
 //                    }
 //                });
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                }catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
             }
+        }
+    }
+
+    public class SocketServer extends WebSocketServer {
+
+        String SOCKET_TAG = "SocketServer";
+
+        public SocketServer(InetSocketAddress address){
+            super(address);
+        }
+
+        @Override
+        public void onOpen(org.java_websocket.WebSocket conn, ClientHandshake handshake) {
+            Log.e(SOCKET_TAG, " Server Listening...");
+        }
+
+        @Override
+        public void onClose(org.java_websocket.WebSocket conn, int code, String reason, boolean remote) {
+            Log.e(SOCKET_TAG, " Server Closed");
+        }
+
+        @Override
+        public void onMessage(org.java_websocket.WebSocket conn, String message) {
+            Log.e(SOCKET_TAG, " Server Message: " + message);
+            conn.send("Thank you");
+        }
+
+        @Override
+        public void onMessage(WebSocket conn, ByteBuffer message) {
+            //super.onMessage(conn, message);
+            byte[] bytes = new byte[message.capacity()];
+        }
+
+        @Override
+        public void onError(org.java_websocket.WebSocket conn, Exception ex) {
+            Log.e(SOCKET_TAG, " Server Error");
+            ex.printStackTrace();
+        }
+
+        @Override
+        public void onStart() {
+
         }
     }
 }
